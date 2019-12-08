2845feace893aaf49f86ef57d501751fcafdb9a3
D2Check <misterfyxit@gmail.com>
2019-12-08 13:24:54 -0500

    changed to a vector system instead of slope and y-intercept. Thanks to Phflack on discord. Bug fixes for the logic.

--- vision_test.py
+++ vision_test.py
@@ -1,8 +1,14 @@
-import math
 from itertools import permutations
 import sys
-
-
+import random
+from player import Player
+import sys
+import time
+import colorama
+import math
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+import numpy as np
 def print_board(board, size):
     board_string = "  "
     board_string += "".join(["{:2}".format(e) for e in range(0, 20)])
@@ -27,23 +33,23 @@ def add_cross(board, x, y):
 import time
 
 
-def area_of_triangle(x1, y1, x2, y2, x3, y3):
+def __area_of_triangle(x1, y1, x2, y2, x3, y3):
     return abs((x1 * (y2 - y3) + x2 * (y3 - y1)
                 + x3 * (y1 - y2)) / 2.0)
 
 
-def isInside(x1, y1, x2, y2, x3, y3, x, y):
+def __is_inside_triangle(x1, y1, x2, y2, x3, y3, x, y):
     # Calculate area of triangle ABC
-    A = area_of_triangle(x1, y1, x2, y2, x3, y3)
+    A = __area_of_triangle(x1, y1, x2, y2, x3, y3)
 
     # Calculate area of triangle PBC
-    A1 = area_of_triangle(x, y, x2, y2, x3, y3)
+    A1 = __area_of_triangle(x, y, x2, y2, x3, y3)
 
     # Calculate area of triangle PAC
-    A2 = area_of_triangle(x1, y1, x, y, x3, y3)
+    A2 = __area_of_triangle(x1, y1, x, y, x3, y3)
 
     # Calculate area of triangle PAB
-    A3 = area_of_triangle(x1, y1, x2, y2, x, y)
+    A3 = __area_of_triangle(x1, y1, x2, y2, x, y)
 
     # Check if sum of A1, A2 and A3
     # is same as A
@@ -54,25 +60,36 @@ def isInside(x1, y1, x2, y2, x3, y3, x, y):
 
 
 def get_sight(board, x, y, size, cross_centers):
+    originalx,originaly = x,y
     for center in cross_centers:
         centx, centy = center
+        # print(f"Working on ({centx},{centy})")
         # Get the points of the cross
-        pnts = [(centx + 1, centy), (centx, centy + 1), (centx - 1, centy), (centx, centy - 1)]
+        pnts = [(centx + 1, centy), (centx, centy + 1),
+                (centx - 1, centy), (centx, centy - 1)]
         to_check = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-        pnts_check = [tuple(e) for e in pnts]
         touching = False
-        if set(pnts_check) & set(to_check):
+        pnts_touching = []
+        if set(pnts) & set(to_check):
             for pnt in pnts:
+                save = False
                 try:
-                    to_check.remove(tuple(pnt))
+                    to_check.remove(pnt)
+                    save = True
                 except:
                     continue
-            if len(to_check) <= 2:
+                if save:
+                    pnts_touching.append(pnt)
+            if len(to_check) == 2:
                 touching = True
-        if not touching:
+        if touching:
+            # print("Yes touching")
+            save = pnts_touching
+        else:
+            # print("Not touching")
+
             d = None
             to_elim = None
-            # eliminate the closest point
             for pnt in pnts:
                 px, py = pnt
                 dist = math.sqrt((px - x) ** 2 + (py - y) ** 2)
@@ -80,8 +97,6 @@ def get_sight(board, x, y, size, cross_centers):
                     d = dist
                     to_elim = pnt
             pnts.remove(to_elim)
-
-            # for all permutations of points, find the largest triangle made with the player
             perms = permutations(pnts, 2)
             area = None
             save = None
@@ -90,175 +105,125 @@ def get_sight(board, x, y, size, cross_centers):
                 ax, ay = a
                 bx, by = b
                 cx, cy = x, y
-                curr_area = area_of_triangle(ax, ay, bx, by, cx, cy)
+                curr_area = __area_of_triangle(ax, ay, bx, by, cx, cy)
                 if area is None or curr_area >= area:
                     area = curr_area
                     save = i
-
-            # for those 2 points, draw a line that goes from player out of the board
-
-
-            # This dict is the point of this function. It will save the details of the large and small triangles, as well
-            # as the slope and y_intercept for the lines
-            triangle_details = {
-                "corner_player": {
-                    "x": x,
-                    "y": y
-                },
-                "cross_corners": [],
-                "line_details": [],
-                "outside_corners": [],
-                "direction": None
-            }
-            # print(centx,x)
-            if centx > x:
-                triangle_details["direction"] = 1
+        triangle_details = {
+            "corner_player": {
+                "x": x,
+                "y": y
+            },
+            "cross_corners": [],
+            "outside_corners": [],
+        }
+        for corner in save:
+            tx, ty = corner
+            # board[tx][ty] = "@"
+            triangle_details['cross_corners'].append(corner)
+            xchange = x - tx
+            ychange = y - ty
+            # print(f"From ({x},{y}) to corner ({tx},{ty}) with vector ({xchange},{ychange})")
+            if xchange == 0 or ychange == 0:
+                newx = x
+                newy = y
             else:
-                triangle_details["direction"] = -1
-            # We need the slope and y_intercept for each of those pnts to the player
-            for corner in save:
-                tx, ty = corner
+                newx = tx
+                newy = ty
+            while True:
+                # if x >= tx:
+                #     newx -=xchange
+                # if y >= ty:
+                #     newy -=ychange
+                # else:
+                #     newy += ychange
 
-                if tx - x != 0:
-                    slope = (ty - y) / (tx - x)
-                    y_int = y - (slope * x)
-                    line = (slope, y_int)
-                    triangle_details["line_details"].append(line)
-                    temp = (tx, ty)
-                    triangle_details["cross_corners"].append(temp)
-                else:
-                    # print(triangle_details["direction"])
-                    # print("This happens")
-                    if triangle_details["direction"] == -1:
-                        slope = float(sys.maxsize)
-                        y_int = float(sys.maxsize)
+                if ychange == 0:
+                    # kill = True
+                    if xchange > 0:
+                        newx = -1 * size
                     else:
-                        slope = float(sys.maxsize)
-                        y_int = float(sys.maxsize)
-                    line = (slope, y_int)
-                    temp = (tx, ty)
-                    triangle_details["cross_corners"].append(temp)
-                    triangle_details["line_details"].append(line)
-                    if centy > y:
-                        for i in range(size - 1, centy, -1):
-                            board[x][i] = " "
-                    else:
-                        # print("yes to here")
-                        pass
-                        # for i in range(0,centy):
-                        #     board[x][i] = " "
-            # using that slope and y_intercept. find 1 point each line that is OUTSIDE of the board
-            ctr = 0
-            for cross_pnt in triangle_details["cross_corners"]:
-                tx, ty = cross_pnt
-                slope, y_int = triangle_details["line_details"][ctr]
-                if triangle_details["direction"] == -1:
-                    for j in range(0, -51, -1):
-                        k = j * slope + y_int
-                        if k.is_integer():
-                            triangle_details["outside_corners"].append((j, k))
-                            break
+                        newx = size * size
                 else:
-                    for j in range(size, size + 50):
-                        k = j * slope + y_int
-                        if k.is_integer():
-                            triangle_details["outside_corners"].append((j, k))
-                            break
-                ctr += 1
-            # We have now found the 5 points we need
-            small_triangle = (triangle_details["corner_player"]["x"],
-                              triangle_details["corner_player"]["y"],
-                              triangle_details["cross_corners"][0][0],
-                              triangle_details["cross_corners"][0][1],
-                              triangle_details["cross_corners"][1][0],
-                              triangle_details["cross_corners"][1][1],
-                              )
-            large_triangle = (triangle_details["corner_player"]["x"],
-                              triangle_details["corner_player"]["y"],
-                              triangle_details["outside_corners"][0][0],
-                              triangle_details["outside_corners"][0][1],
-                              triangle_details["outside_corners"][1][0],
-                              triangle_details["outside_corners"][1][1],
-                              )
+                    if not touching:
+                        newx -= xchange
 
-            # print(triangle_details)
-            # for every location on the board
-            for testy in range(size):
-                for testx in range(size):
-                    ax, ay, bx, by, cx, cy = small_triangle
-                    inside_small = isInside(ax, ay, bx, by, cx, cy, testx, testy)
-                    ax, ay, bx, by, cx, cy = large_triangle
-                    inside_large = isInside(ax, ay, bx, by, cx, cy, testx, testy)
-                    # if that point is inside the large triangle and not inside the small triangle we cant see there
-                    if inside_large and not inside_small and board[testx][testy]:
-                        board[testx][testy] = " "
+                if xchange == 0:
+                    # kill = True
+                    if ychange > 0:
+                        newy = -1 * size
+                    else:
+                        newy = size * size
+                else:
+                    if not touching:
+                        newy -= ychange
 
-        else:
-            # some points ARE touching
-            # print("Points are touching")
-            # print(f"player at ({x},{y})")
-            # print(pnts)
-            up, down, left, right = False, False, False, False
+                # print(f"Eval ({newx},{newy})")
+                if (newx < 0 or newx >= size) or (newy < 0 or newy >= size):
+                    break
+            outside_corner = (newx, newy)
+            # print(f"Adding {outside_corner} as an outside corner")
 
-            me = board[x][y]
-            if (x - 1, y) in pnts:
-                left = True
-            if (x + 1, y) in pnts:
-                right = True
-            if (x, y + 1) in pnts:
-                down = True
-            if (x, y - 1) in pnts:
-                up = True
-            # print(up,down,left,right)
-            if down and left:
-                for k in range(y, size):
-                    for j in range(x, -1, -1):
-                        board[j][k] = " "
-                board[x - 1][y] = "#"
-                board[x - 1][y + 1] = "#"
-                board[x][y + 1] = "#"
-                board[x][y] = me
+            triangle_details['outside_corners'].append(outside_corner)
+        # print(triangle_details)
+        small_triangle = (triangle_details["corner_player"]["x"],
+                          triangle_details["corner_player"]["y"],
+                          triangle_details["cross_corners"][0][0],
+                          triangle_details["cross_corners"][0][1],
+                          triangle_details["cross_corners"][1][0],
+                          triangle_details["cross_corners"][1][1],
+                          )
+        large_triangle = (triangle_details["corner_player"]["x"],
+                          triangle_details["corner_player"]["y"],
+                          triangle_details["outside_corners"][0][0],
+                          triangle_details["outside_corners"][0][1],
+                          triangle_details["outside_corners"][1][0],
+                          triangle_details["outside_corners"][1][1],
+                          )
+        for testy in range(size):
+            for testx in range(size):
+                ax, ay, bx, by, cx, cy = small_triangle
+                inside_small = __is_inside_triangle(
+                    ax, ay, bx, by, cx, cy, testx, testy)
+                ax, ay, bx, by, cx, cy = large_triangle
+                inside_large = __is_inside_triangle(
+                    ax, ay, bx, by, cx, cy, testx, testy)
+                # if that point is inside the large triangle and not inside the small triangle we cant see there
+                if inside_large and not inside_small and board[testx][testy]:
+                    board[testx][testy] = " "
+    # if kill:
+    # string_board = np.array(board).T
+    # float_board = np.zeros(string_board.shape + (3,))
+    # switch = {
+    #     '@': (0, 1, 1),
+    #     ' ': (0, 0, 0),
+    #     '.': (0.5, 0.5, 0.5),
+    #     '#': (0.2, 0.2, 0.2),
+    #     '1': (1, 0, 0),
+    #     '2': (0, 1, 0)
+    # }
+    # for index, value in np.ndenumerate(string_board):
+    #     x, y = index
+    #     float_board[x][y] = switch[value]
+    # fig, ax = plt.subplots(1)
+    # ax.imshow(float_board)
+    # for center in cross_centers:
+    #     ax.add_patch(Circle(center, radius=0.4, color='blue'))
+    # plt.show()
+    #     sys.exit()
 
-            if right and down:
-                for k in range(y, size):
-                    for j in range(x, size):
-                        board[j][k] = " "
-                board[x + 1][y] = "#"
-                board[x + 1][y + 1] = "#"
-                board[x][y + 1] = "#"
-                board[x][y] = me
-            if up and right:
-                for k in range(y, -1, -1):
-                    for j in range(x, size):
-                        board[j][k] = " "
-                board[x + 1][y] = "#"
-                board[x + 1][y - 1] = "#"
-                board[x][y - 1] = "#"
-                board[x][y] = me
-            if up and left:
-                for k in range(y, -1, -1):
-                    for j in range(x, -1, -1):
-                        board[j][k] = " "
-                board[x - 1][y] = "#"
-                board[x - 1][y - 1] = "#"
-                board[x][y - 1] = "#"
-                board[x][y] = me
     return board
 
 
 size = 20
 board = [["."] * size for i in range(size)]
 board[0][0] = "1"
-p2x, p2y = 5, 2
+p2x, p2y = 9, 1
 board[p2x][p2y] = "2"
-cross_centers = [(4, 3),(6,6)]
+cross_centers = [(6,3),(15,17)]
 for cr in cross_centers:
     x, y = cr
     board = add_cross(board, x, y)
-board[6][3] = "x"
 print_board(board, 20)
-for x in range(p2x,p2x+1):
-    for y in range(p2y+1,p2y+4):
-        print(board[x][y])
-# board = get_sight(board, p2x, p2y, 20, cross_centers)
-# print_board(board, 20)
+board = get_sight(board, p2x, p2y, 20, cross_centers)
+print_board(board, 20)
